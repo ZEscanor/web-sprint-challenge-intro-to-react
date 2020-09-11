1b6c9cd8cffa278c4cecf8a22e517863e1080f32
Yinusa <yamusa2@uic.edu>
2020-09-11 14:17:47 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import './App.css';
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+//import axios from "axios";
+import styled from "styled-components";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,12 +12,52 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  //console.log(axios.get(`https://swapi.dev/api/people/1/`));
+  //const apikey= 1;
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+  
+const StyledApp = styled.div`
+ display:flex;
+ flex-flow:column;
+ 
+`;
+const StyledDiv = styled.div`
+display: flex;
+flex-flow: row;
+flex-wrap:wrap;
+`;
+
+  const [user, setUser] = useState([]);
+  
+ useEffect(()=>{
+   fetch("https://swapi.dev/api/people/")
+    .then(response=>response.json())
+    .then(user=>{
+      console.log("fetch",user);
+      setUser(user.results);
+      //setImage(user.hdurl);
+    })
+ },[])
+
+console.log(user.results);
+      if(!user){
+        return <p>Loading.. </p>
+      }
+      else{
+  return(
+    
+    <StyledApp className="App">
+      <h1 className="Header">React Wars</h1>
+      
+      <StyledDiv>  {user.map((name) =>{
+        console.log("ThIS IS MAP", name);
+       return <Character user={name}/>;
+      })};</StyledDiv>
+    
+  
+         </StyledApp>
+         )
+      };
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React from 'react';
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+display:flex;
+  background-color: black;
+  color:white;
+  margin: 3%;
+  padding: 2%;
+  border: 3px solid green;
+`;
+
+const StyledButton = styled.button`
+ margin-left: 50px;
+ 
+`
+
+const Character = (props => {
+  
+    const {user} = props;
+    console.log("thisUSer", user);
+    return(
+            <StyledDiv  className = "header">
+            {user.name} 
+            <StyledButton>
+                {user.birth_year}; 
+            </StyledButton>
+    
+            </StyledDiv>
+            
+        
+        )
+            
+    });
+
+export default Character;
